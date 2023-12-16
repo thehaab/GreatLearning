01506dba1bcd5e0fa03f6057fe2def5234cfa5fb
thehaab <thehaab@gmail.com>
2023-12-16 10:45:46 -0600

    Add files via upload

---
+++ functionCallContext.js
@@ -0,0 +1,18 @@
+function foo() {
+  console.log(this);
+}
+
+//foo();
+
+var john = {
+  name: 'John',
+  getName: function() {
+    console.log(this);
+    return this.name;
+  }
+};
+
+console.log(john.getName())
+
+var getName = john.getName;
+getName();
